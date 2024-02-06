105a92805ddf6fc092f0387eabac713af7a50ff8
Stoyan Banov <sbanov05@gmail.com>
2024-02-06 15:55:44 +0200

    add js advanced exam

---
+++ Js_Applications/01_Asynchronous_Programming/06_Blog/app.js
@@ -0,0 +1,95 @@
+function attachEvents() {
+    const postsSelect = document.getElementById('posts'),
+        postH1 = document.getElementById('post-title'),
+        postP = document.getElementById('post-body'),
+        commentsUl = document.getElementById('post-comments'),
+        host = 'http://localhost:3030/jsonstore/blog'
+
+    let posts
+
+    document.getElementById('btnLoadPosts').addEventListener('click', async () => {
+        posts = await get('/posts')
+
+        postsSelect.innerHTML = ''
+        for (const [k, v] of Object.entries(posts)) {
+            postsSelect.appendChild(makePostOption(k, v.title))
+        }
+    })
+
+    document.getElementById('btnViewPost').addEventListener('click', async () => {
+        const [post, comments] = await Promise.all([get('/posts/' + posts[postsSelect.value].id), get('/comments')])
+
+        postH1.textContent = post.title
+        postP.textContent = post.body
+
+        commentsUl.innerHTML = ''
+        for (let c of Object.values(comments).filter(c => c.postId == post.id)) {
+            commentsUl.appendChild(makeCommentLi(c.text))
+        }
+    })
+
+    function makePostOption(value, text) {
+        const option = document.createElement('option')
+        option.value = value
+        option.text = text
+        return option
+    }
+
+    function makeCommentLi(text) {
+        const li = document.createElement('li')
+        li.textContent = text
+        return li
+    }
+
+    async function get(url) {
+        return (await fetch(host + url)).json()
+    }
+}
+
+
+//version for judje
+
+// function attachEvents() {
+//     const postsSelect = document.getElementById('posts'),
+//         postH1 = document.getElementById('post-title'),
+//         postP = document.getElementById('post-body'),
+//         commentsUl = document.getElementById('post-comments'),
+//         host = 'http://localhost:3030/jsonstore/blog'
+
+//     let posts
+
+//     document.getElementById('btnLoadPosts').addEventListener('click', async () => {
+//         posts = await get('/posts')
+
+//         postsSelect.innerHTML = ''
+//         for (const p of posts) {
+//             postsSelect.appendChild(makePostOption(p.id, p.title))
+//         }
+//     })
+
+//     document.getElementById('btnViewPost').addEventListener('click', async () => {
+//         const post = await get('/posts/' + postsSelect.value)
+
+//         postH1.textContent = post[0].title
+//         postP.textContent = post[0].body
+//     })
+
+//     function makePostOption(value, text) {
+//         const option = document.createElement('option')
+//         option.value = value
+//         option.text = text
+//         return option
+//     }
+
+//     function makeCommentLi(text) {
+//         const li = document.createElement('li')
+//         li.textContent = text
+//         return li
+//     }
+
+//     async function get(url) {
+//         return (await fetch(host + url)).json()
+//     }
+// }
+
+attachEvents();
