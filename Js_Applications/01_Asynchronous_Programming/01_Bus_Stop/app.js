f876c1228ef2390555df34ecef84264cfd3bce3b
Stoyan Banov <sbanov05@gmail.com>
2024-02-08 22:19:03 +0200

    add async programming task

--- Js_Applications/01_Asynchronous_Programming/01_Bus_Stop/app.js
+++ Js_Applications/01_Asynchronous_Programming/01_Bus_Stop/app.js
@@ -1,3 +1,17 @@
-function getInfo() {
-    console.log("TODO...");
+const stopIdInput = document.getElementById('stopId'),
+    stopNameDiv = document.getElementById('stopName'),
+    busesUl = document.getElementById('buses')
+
+async function getInfo() {
+    try {
+        const stop = await (await fetch('http://localhost:3030/jsonstore/bus/businfo/' + stopIdInput.value)).json()
+
+        stopNameDiv.textContent = stop.name
+
+        busesUl.innerHTML = Object.entries(stop.buses)
+            .map(([k, v]) => `<li>Bus ${k} arrives in ${v} minutes</li>`)
+            .join('\n')
+    } catch (error) {
+        stopNameDiv.textContent = 'Error'
+    }
 }
