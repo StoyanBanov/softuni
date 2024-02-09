42d2bd2051b8bd2cef7627912221a36f6131534d
Stoyan Banov <sbanov05@gmail.com>
2024-02-09 22:20:01 +0200

    add async programming task

--- Js_Applications/01_Asynchronous_Programming/02_Bus_Schedule/app.js
+++ Js_Applications/01_Asynchronous_Programming/02_Bus_Schedule/app.js
@@ -1,11 +1,24 @@
 function solve() {
+    const infoBox = document.querySelector('.info'),
+        departBtn = document.getElementById('depart'),
+        arriveBtn = document.getElementById('arrive')
 
-    function depart() {
-        console.log('Depart TODO...');
+    let stop = { next: 'depot' }
+
+    async function depart() {
+        stop = await (await fetch(`http://localhost:3030/jsonstore/bus/schedule/${stop.next}`)).json()
+
+        infoBox.textContent = `Next stop ${stop.name}`
+
+        departBtn.disabled = true
+        arriveBtn.disabled = false
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        infoBox.textContent = `Arriving at ${stop.name}`
+
+        arriveBtn.disabled = true
+        departBtn.disabled = false
     }
 
     return {
